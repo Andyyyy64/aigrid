e393a9861b495577d3ed739d459bd02b33574f43
andy <takuyarossi77@gmail.com>
2024-06-03 15:36:52 +0900

    init

---
+++ main/main.c
@@ -0,0 +1,10 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_log.h"
+#include "esp_http_client.h"
+
+void app_main() {
+    
+}
